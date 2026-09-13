1b56cbd0ec75149db7bbff6c76cd92c20baf754f
Marvin <mcanastuj1999@gmail.com>
2026-09-13 00:06:47 -0600

    fixing issue #1 keep the footer at the end of the viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     }) {
       return (
         <html lang="en">
-          <body>
+          <body className="border h-screen flex flex-col justify-between">
             <Header />
             {children}
             <Footer />
